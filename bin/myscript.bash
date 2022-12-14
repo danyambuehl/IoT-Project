06c06442ca67bec777046e6477e5bf119aa1cf19
Dany <dany.ambuehl1@swisscom.com>
2022-12-14 11:18:03 +0100

    variable

--- bin/myscript.bash
+++ bin/myscript.bash
@@ -8,3 +8,9 @@ echo "Date: $(date +%H:%M:%S)" 1>> $htmloutput
 date +%H:%M:%S 1>> $htmloutput
 echo "Listet den Inhalt von Temp Verzeichniss:\n $(ls $DIRECTORYTOLIST)" 1>> $htmloutput
 echo "</body></html>" 1>> $htmloutput
+echo $0
+echo $1 
+echo $2 
+echo $3
+echo $*
+echo $#
