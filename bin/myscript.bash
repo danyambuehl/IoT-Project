676a902af7f498d631a02c8b3384f240a4a330a5
Dany <dany.ambuehl1@swisscom.com>
2022-12-14 10:54:45 +0100

    berechtigung

--- bin/myscript.bash
+++ bin/myscript.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 htmloutput=/opt/iotproject/index.html
-
+DIRECTORYTOLIST=/tmp
 echo "<html><body>" 1> $htmloutput
 echo "<h1>Mein Webserver</h1>" 1>> $htmloutput
 echo "Date: $(date +%H:%M:%S)" 1>> $htmloutput
 date +%H:%M:%S 1>> $htmloutput
-echo "Listet den Inhalt von Temp Verzeichniss:\n $(ls /tmp)" 1>> $htmloutput
+echo "Listet den Inhalt von Temp Verzeichniss:\n $(ls $DIRECTORYTOLIST)" 1>> $htmloutput
 echo "</body></html>" 1>> $htmloutput
