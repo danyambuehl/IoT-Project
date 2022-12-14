e57c4593eb23300622c49846fbd51978a92acde9
Dany <dany.ambuehl1@swisscom.com>
2022-12-14 10:37:47 +0100

    Variable

--- bin/myscript.bash
+++ bin/myscript.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
+htmloutput=/opt/iotproject/index.html
 
-echo "<html><body>" 1> index.html
-echo "<h1>Mein Webserver</h1>" 1>> index.html
-echo "Date: $(date +%H:%M:%S)" 1>> index.html
-date +%H:%M:%S 1>> index.html
-echo "Listet den Inhalt von Temp Verzeichniss:\n $(ls /tmp)" 1>> index.html
-echo "</body></html>" 1>> index.html
+echo "<html><body>" 1> $htmloutput
+echo "<h1>Mein Webserver</h1>" 1>> $htmloutput
+echo "Date: $(date +%H:%M:%S)" 1>> $htmloutput
+date +%H:%M:%S 1>> $htmloutput
+echo "Listet den Inhalt von Temp Verzeichniss:\n $(ls /tmp)" 1>> $htmloutput
+echo "</body></html>" 1>> $htmloutput
