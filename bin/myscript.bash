7df5bd2fbb619a52d81981fe50401345beb57d40
root <root@ip-172-31-87-208.ec2.internal>
2022-12-14 09:31:10 +0000

    myscript

--- bin/myscript.bash
+++ bin/myscript.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "<html><body>"
-echo "<h1>Mein Webserver</h1>"
-echo "</body></html>"
+echo "<html><body>" 1> index.html
+echo "<h1>Mein Webserver</h1>" 1>> index.html
+echo "Date: $(date +%H:%M:%S)" 1>> index.html
+date +%H:%M:%S 1>> index.html
+echo "Listet den Inhalt von Temp Verzeichniss:\n $(ls /tmp)" 1>> index.html
+echo "</body></html>" 1>> index.html
