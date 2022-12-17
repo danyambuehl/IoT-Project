1d4a0fc6fb18bbe105236d72ee644a712597f5a5
Dany <dany.ambuehl1@swisscom.com>
2022-12-17 15:00:47 +0100

    teilung meiner scirpts

--- bin/loop.bash
+++ bin/loop.bash
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-DIRECTORYTOLIST=/tmp # this is the default
-
-if [ $# -ge 1 ]
-then
-    if [ -d "$1" ] 
-    then
-        DIRECTORYTOLIST=$1
-    else
-        echo "Directory $1 not found."
-        exit 52
-    fi
-fi
-shift 
-
-ls $DIRECTORYTOLIST
-
 # For loop file prüfen und anhängen oder fehler
 
 for htmlsnipped in $*
