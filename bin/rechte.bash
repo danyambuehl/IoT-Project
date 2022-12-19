5ebcc63842e6eec8ee41fe09f6b3550180989abe
Dany <dany.ambuehl1@swisscom.com>
2022-12-19 13:46:55 +0100

    webservice

--- bin/rechte.bash
+++ bin/rechte.bash
@@ -1,10 +1,22 @@
 #!/bin/bash
-INTERVAL=30
-while true 
+# /opt/iotproject
+
+INTERVAL=10
+# ls -lar *bash | awk '{print $1}'
+ls -lar *.bash > rechte.html
+rechte="cat rechte.html|cut -d " " -f 1" 
+richtig="-rwxr-xr-x"
+
+if [ $rechte != $richtig ]
+    echo "muss angepasst werden"
+fi
+
+
+while [$rechte != $richtig] 
 do
 for what in /opt/iotproject/bin/*.bash
 	do 
-	# echo $what
+	echo $what
 	ls -l $what
 	chmod +x $what
 	ls -l $what 
