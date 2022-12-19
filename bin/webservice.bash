d069421ad24e4f1e1dac14ea312710ceab0e5183
Dany <dany.ambuehl1@swisscom.com>
2022-12-19 13:47:14 +0100

    web service

---
+++ bin/webservice.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TEMPLATEHTMLFILE=/opt/iotproject/etc/index.html.template 
+INTERVAL=20
+HTMLOUTPUT=/opt/iotproject/index.html
+
+counter=0
+while [ $counter -lt 20 ] 
+    do
+        zeit=$(date)
+        # sed = Suchen und ersetzen s/ ersetzen _Zeit_ = was $zeit = mit was /g = auf einer zeile
+        cat $TEMPLATEHTMLFILE | sed "s/_ZEIT_/$zeit/g" > $HTMLOUTPUT
+        counter=$((counter+1))
+    done
